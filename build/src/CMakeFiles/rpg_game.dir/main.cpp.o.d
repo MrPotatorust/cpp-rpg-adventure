47c0f2cf77b09bb4d4c60ee13a991aabea4a41a7
Martin Jaros <martinjaros.personal@gmail.com>
2026-09-01 12:39:19 +0200

    remove build from repo

--- build/src/CMakeFiles/rpg_game.dir/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/rpg_game.dir/main.cpp.o: \
- /home/martinj/personal_projects/fun/rpg/src/main.cpp \
- /usr/include/stdc-predef.h
--- build/tests/hello_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/martinj/personal_projects/fun/rpg/build/tests/hello_test[1]_tests.cmake")
-  include("/home/martinj/personal_projects/fun/rpg/build/tests/hello_test[1]_tests.cmake")
-else()
-  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
-endif()
