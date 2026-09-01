9eee2195541ac3292ed151a0d8b4f038bd6c0f37
Martin Jaros <martinjaros.personal@gmail.com>
2026-09-01 12:38:24 +0200

    first commit

---
+++ build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/martinj/personal_projects/fun/rpg/build/_deps/googletest-src
+# Build directory: /home/martinj/personal_projects/fun/rpg/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/src/CMakeFiles/rpg_game.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/rpg_game.dir/main.cpp.o: \
+ /home/martinj/personal_projects/fun/rpg/src/main.cpp \
+ /usr/include/stdc-predef.h
